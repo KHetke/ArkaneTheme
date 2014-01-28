2f0f7f4f220f2db436161caed5f972869587c407
Kamran Hetke <KHetke.Balticorp@gmail.com>
2014-01-28 13:00:23 -0800

    Removed the CallToAction file, and changed text color, the Navbar, and added a link to monthly archives.

--- arkane2013/footer.php
+++ arkane2013/footer.php
@@ -10,7 +10,6 @@
 	<footer id="colophon" role="contentinfo">
 			<div id="site-generator">
 				<small>&copy Copyright <?php echo date('Y') . " " . esc_attr( get_bloginfo( 'name', 'display' ) ); ?> <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'themename' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'themename' ), 'WordPress' ); ?></a></small>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- .container (Closes first div from header.php) -->
